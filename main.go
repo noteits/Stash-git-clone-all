10c10b26f6225674a1ca308f5138b828d12fb75a
Nazarii Koblai <nazarkoblay@gmail.com>
2017-06-20 12:05:14 +0300

    added concurrency

--- main.go
+++ main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/url"
-
-	"fmt"
-	"github.com/xoom/stash"
 	"os/exec"
 	"strings"
+	"sync"
+
+	"github.com/xoom/stash"
 )
 
+// ExecuteCMD executes cmd command for inputed params.
+func ExecuteCMD(usrName, usrPas, stashHost, prjKey, branch string, p stash.Repository, output chan string, wg *sync.WaitGroup, err error) {
+	defer wg.Done()
+	gitCmd := fmt.Sprintf("%s%s@%s/scm/%s/%s.git", stashHost[:8], usrName, stashHost[8:], strings.ToLower(prjKey), p.Slug)
+	var gc = *exec.Command("git", "clone", "-b", branch, gitCmd)
+	err = gc.Run()
+	if err != nil {
+		output <- err.Error()
+	}
+	output <- gitCmd
+}
+
 func main() {
+
 	var usrName, usrPas, stashHost, prjKey, branch string
+	var p stash.Repository
+	var prjSet map[int]stash.Repository
+	var wg sync.WaitGroup
 
 	flag.StringVar(&stashHost, "bp", "https://some-stash-host.net", "Stash host")
 	flag.StringVar(&usrName, "un", "name", "Stash user name")
@@ -22,38 +39,43 @@ func main() {
 
 	flag.Parse()
 
+	response := make(chan string)
+
 	bu, err := url.Parse(stashHost)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	cl := stash.NewClient(usrName, usrPas, bu)
 
-	var prjSet map[int]stash.Repository
 	prjSet, err = cl.GetRepositories()
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	var gitCmd string
-	var p stash.Repository
-	var gc *exec.Cmd
 
 	for i := range prjSet {
 		p = prjSet[i]
 		if p.Project.Key != prjKey {
-			continue
+			delete(prjSet, i)
 		}
 		//gitCmd = fmt.Sprintf("git clone https://%s@%s/scm/%s/%s.git", usrName, stashHost, prjKey, p.Slug)
 		//println(gitCmd)
 
-		gitCmd = fmt.Sprintf("%s%s@%s/scm/%s/%s.git", stashHost[:8], usrName, stashHost[8:], strings.ToLower(prjKey), p.Slug)
-		println(gitCmd)
-		gc = exec.Command("git", "clone", "-b", branch, gitCmd)
-		err = gc.Run()
-		if err != nil {
-			println(err.Error())
-		}
-
 		//println(prjSet[i].Name, prjSet[i].Slug)
 	}
+
+	(&wg).Add(len(prjSet))
+
+	for i := range prjSet {
+		go ExecuteCMD(usrName, usrPas, stashHost, prjKey, branch, prjSet[i], response, &wg, err)
+	}
+
+	go func() {
+		for i := range response {
+			fmt.Println(i)
+		}
+	}()
+
+	wg.Wait()
 }
